5cd8731a17b9dcb9281fb70d8789f9ef02026692
Alexander Augusto <alexaasf1010@gmail.com>
2022-09-17 16:26:54 -0300

    Add style to application
    - Add CSS structure to home and menu;
    - Add javascript file to make a
    hamburguer menu;
    - Add background with particles.js.

---
+++ javascript/menu.js
@@ -0,0 +1,7 @@
+function showLeftMenu() {
+  document.getElementById('cv-container').classList.add('menu--open')
+}
+
+function hideLeftMenu() {
+  document.getElementById('cv-container').classList.remove('menu--open')
+}
