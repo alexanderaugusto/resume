fb78959eac62a3ff151ea34109d6674e759dd9d9
Alexander Augusto <alexaasf1010@gmail.com>
2022-09-18 10:49:21 -0300

    Add dark theme to project

--- javascript/app.js
+++ javascript/app.js
@@ -5,3 +5,25 @@ function showLeftMenu() {
 function hideLeftMenu() {
   document.getElementById('cv-container').classList.remove('menu--open')
 }
+
+function applyLightTheme() {
+  document.getElementById('bg-container').classList.add('theme--light')
+  document.getElementById('bg-container').classList.remove('theme--dark')
+  document.getElementById('btn-theme-home').innerHTML = '<i class="fas fa-moon"></i>'
+  document.getElementById('btn-theme-menu').innerHTML = '<i class="fas fa-moon"></i>'
+}
+
+function applyDarkTheme() {
+  document.getElementById('bg-container').classList.add('theme--dark')
+  document.getElementById('bg-container').classList.remove('theme--light')
+  document.getElementById('btn-theme-home').innerHTML = '<i class="fas fa-sun"></i>'
+  document.getElementById('btn-theme-menu').innerHTML = '<i class="fas fa-sun"></i>'
+}
+
+function changeTheme() {
+  if (document.getElementById('bg-container').classList.contains('theme--light')) {
+    applyDarkTheme()
+  } else {
+    applyLightTheme()
+  }
+}
